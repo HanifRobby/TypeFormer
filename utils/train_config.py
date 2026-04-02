10f9b3e3711b316005bec81c9dd2b80e96ebbfb0
Hanif Robby <96159381+HanifRobby@users.noreply.github.com>
2026-04-02 20:29:09 +0700

    fix: Update model_filename with correct path

--- utils/train_config.py
+++ utils/train_config.py
@@ -2,13 +2,13 @@ import argparse
 
 configs = argparse.ArgumentParser()
 
-configs.model_name = 'pretrained/preliminary_transformer_pretrained.pt'
+configs.model_name = 'latest_experiment'
 configs.base_dir = configs.model_name + '/'
 configs.model_dir = configs.base_dir
 configs.log_dir = configs.base_dir
 configs.data_dir = 'data/'
 configs.log_filename = configs.log_dir + configs.model_name + '_log.txt'
-configs.model_filename = configs.model_dir + configs.model_name + '.pt'
+configs.model_filename = 'pretrained/preliminary_transformer_pretrained.pt'
 
 
 configs.main_db = configs.data_dir + 'Mobile_keys_db_6_features.npy'
